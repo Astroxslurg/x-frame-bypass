9838b9ba5c658ea294c56aadcb288b4a2d155e4d
Kristian Trandem <kritra@netcompany.com>
2023-03-02 14:38:27 +0100

    Replace CORS proxies with some that might work. Also put strings in one line, because I think that might have cause some problems for me.

--- x-frame-bypass.js
+++ x-frame-bypass.js
@@ -15,63 +15,17 @@ customElements.define('x-frame-bypass', class extends HTMLIFrameElement {
 		if (!url || !url.startsWith('http'))
 			throw new Error(`X-Frame-Bypass src ${url} does not start with http(s)://`)
 		console.log('X-Frame-Bypass loading:', url)
-		this.srcdoc = `<html>
-<head>
-	<style>
-	.loader {
-		position: absolute;
-		top: calc(50% - 25px);
-		left: calc(50% - 25px);
-		width: 50px;
-		height: 50px;
-		background-color: #333;
-		border-radius: 50%;  
-		animation: loader 1s infinite ease-in-out;
-	}
-	@keyframes loader {
-		0% {
-		transform: scale(0);
-		}
-		100% {
-		transform: scale(1);
-		opacity: 0;
-		}
-	}
-	</style>
-</head>
-<body>
-	<div class="loader"></div>
-</body>
-</html>`
+		this.srcdoc = `<html><head><style>.loader{position:absolute;top:calc(50% - 25px);left:calc(50% - 25px);width:50px;height:50px;background-color:#333;border-radius:50%;animation:loader 1s infinite ease-in-out}@keyframes loader{0%{transform:scale(0)}100%{transform:scale(1);opacity:0}}</style></head><body><div class="loader"></div></body></html>`
 		this.fetchProxy(url, options, 0).then(res => res.text()).then(data => {
 			if (data)
-				this.srcdoc = data.replace(/<head([^>]*)>/i, `<head$1>
-	<base href="${url}">
-	<script>
-	// X-Frame-Bypass navigation event handlers
-	document.addEventListener('click', e => {
-		if (frameElement && document.activeElement && document.activeElement.href) {
-			e.preventDefault()
-			frameElement.load(document.activeElement.href)
-		}
-	})
-	document.addEventListener('submit', e => {
-		if (frameElement && document.activeElement && document.activeElement.form && document.activeElement.form.action) {
-			e.preventDefault()
-			if (document.activeElement.form.method === 'post')
-				frameElement.load(document.activeElement.form.action, {method: 'post', body: new FormData(document.activeElement.form)})
-			else
-				frameElement.load(document.activeElement.form.action + '?' + new URLSearchParams(new FormData(document.activeElement.form)))
-		}
-	})
-	</script>`)
+				this.srcdoc = data.replace(/<head([^>]*)>/i, `<head $1><base href="${url}"><script>document.addEventListener("click",e=>{frameElement&&document.activeElement&&document.activeElement.href&&(e.preventDefault(),frameElement.load(document.activeElement.href))});document.addEventListener("submit",e=>{frameElement&&document.activeElement&&document.activeElement.form&&document.activeElement.form.action&&(e.preventDefault(),"post"===document.activeElement.form.method?frameElement.load(document.activeElement.form.action,{method:"post",body:new FormData(document.activeElement.form)}):frameElement.load(document.activeElement.form.action+"?"+new URLSearchParams(new FormData(document.activeElement.form))))});</script>`)
 		}).catch(e => console.error('Cannot load X-Frame-Bypass:', e))
 	}
 	fetchProxy (url, options, i) {
 		const proxies = (options || {}).proxies || [
-			'https://cors-anywhere.herokuapp.com/',
-			'https://yacdn.org/proxy/',
-			'https://api.codetabs.com/v1/proxy/?quest='
+			'https://proxy.cors.sh/',
+			'https://test.cors.workers.dev/?',
+			'https://api.allorigins.win/raw?url='
 		]
 		return fetch(proxies[i] + url, options).then(res => {
 			if (!res.ok)
